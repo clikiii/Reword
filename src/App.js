2946e39d1f1f5b17e884d357aed26729c8f54455
clikiii <18019349217@163.com>
2023-09-12 13:02:14 +0800

    feat: idx and app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
-function App() {
+import Home from './pages/Home';
+import WordList from './pages/WordList';
+
+const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/list" element={ <WordList /> } />
+      </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
